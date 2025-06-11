c9c09e0de2af5cd5d98cfe003a55e26062d1d9f9
Nguyen Minh Nhat <minhtientiny@gmail.com>
2025-06-11 14:30:43 +0700

    hi

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,36 +10,38 @@ export const Hero = () => {
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              Shadcn
+              Đậu
             </span>{" "}
-            landing page
+            Hủ
           </h1>{" "}
           for{" "}
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              React
+              Yummy
             </span>{" "}
-            developers
+            XP
           </h2>
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Build your React landing page effortlessly with the required sections
-          to your project.
+          Tạo Nên Sự Lười Biến Cho Bạn, Và Sự Giàu Có Cho Tôi Và Bạn, Trình Thực Thi Script Roblox PC Miễn Phí, Đến Từ Việt Nam.
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Get Started</Button>
+  <Link href="/get-started" className="w-full md:w-1/3">
+    <Button className="w-full">Bắt Đầu</Button>
+  </Link>
+</div>
 
           <a
             rel="noreferrer noopener"
-            href="https://github.com/leoMirandaa/shadcn-landing-page.git"
+            href="3gg"
             target="_blank"
             className={`w-full md:w-1/3 ${buttonVariants({
               variant: "outline",
             })}`}
           >
-            Github Repository
+            Tham Gia Discord
             <GitHubLogoIcon className="ml-2 w-5 h-5" />
           </a>
         </div>
