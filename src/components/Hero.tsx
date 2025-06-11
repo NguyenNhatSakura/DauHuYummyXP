4be6559f967d021920d69951bd8ee57222d4fcae
Nguyen Minh Nhat <minhtientiny@gmail.com>
2025-06-11 14:37:07 +0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "./ui/button";
 import { buttonVariants } from "./ui/button";
 import { HeroCards } from "./HeroCards";
@@ -8,7 +9,7 @@ export const Hero = () => {
       <div className="text-center lg:text-start space-y-6">
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
+            <span className="inline bg-gradient-to-r from-[#F596D3] to-[#D247BF] text-transparent bg-clip-text">
               Đậu
             </span>{" "}
             Hủ
@@ -26,26 +27,23 @@ export const Hero = () => {
           Tạo Nên Sự Lười Biến Cho Bạn, Và Sự Giàu Có Cho Tôi Và Bạn, Trình Thực Thi Script Roblox PC Miễn Phí, Đến Từ Việt Nam.
         </p>
 
-        <div className="space-y-4 md:space-y-0 md:space-x-4">
-  <Link href="/get-started" className="w-full md:w-1/3">
-    <Button className="w-full">Bắt Đầu</Button>
-  </Link>
-</div>
+        <div className="flex flex-col md:flex-row gap-4">
+          <a href="/get-started" className="w-full md:w-1/3">
+            <Button className="w-full">Bắt Đầu</Button>
+          </a>
 
           <a
             rel="noreferrer noopener"
             href="3gg"
             target="_blank"
-            className={`w-full md:w-1/3 ${buttonVariants({
-              variant: "outline",
-            })}`}
+            className={`w-full md:w-1/3 ${buttonVariants({ variant: "outline" })}`}
           >
             Tham Gia Discord
           </a>
         </div>
       </div>
 
-      {/* Hero cards sections */}
+      {/* Hero cards section */}
       <div className="z-10">
         <HeroCards />
       </div>
