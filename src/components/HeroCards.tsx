9f9d3c4330606a691413cf8957df269d1199843f
Nguyen Minh Nhat <minhtientiny@gmail.com>
2025-06-11 15:10:02 +0700

    Update HeroCards.tsx

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -70,31 +70,6 @@ export const HeroCards = () => {
               <span className="sr-only">Github icon</span>
               <GitHubLogoIcon className="w-5 h-5" />
             </a>
-            <a
-              rel="noreferrer noopener"
-              href="https://fb.com/nguyennhat.dev"
-              target="_blank"
-              className={buttonVariants({
-                variant: "ghost",
-                size: "sm",
-              })}
-            >
-              <span className="sr-only">Facebook icon</span>
-              <svg width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><title>Facebook icon</title><path d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z"/></svg>
-            </a>
-
-            <a
-              rel="noreferrer noopener"
-              href="https://www.youtube.com/@dauhuyummychannel"
-              target="_blank"
-              className={buttonVariants({
-                variant: "ghost",
-                size: "sm",
-              })}
-            >
-              <span className="sr-only">Youtube icon</span>
-              <svg fill="#000000" width="800px" height="800px" viewBox="0 0 24 24" id="youtube" data-name="Line Color" xmlns="http://www.w3.org/2000/svg" class="icon line-color"><polygon id="secondary" points="10 9 10 15 15 12 10 9" style="fill: none; stroke: rgb(44, 169, 188); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></polygon><path id="primary" d="M21,15.8a3,3,0,0,1-2.76,3c-1.49.11-3.56.21-6.24.21s-4.75-.1-6.24-.21A3,3,0,0,1,3,15.8V8.2a3,3,0,0,1,2.76-3C7.25,5.1,9.32,5,12,5s4.75.1,6.24.21A3,3,0,0,1,21,8.2Z" style="fill: none; stroke: rgb(0, 0, 0); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></svg>
-            </a>
           </div>
         </CardFooter>
       </Card>
