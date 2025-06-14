54d8aa4ed377afc9c0c354b1aca9838b4be73c5b
Nguyen Minh Nhat <minhtientiny@gmail.com>
2025-06-14 15:42:21 +0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,6 +11,7 @@ export const Hero = () => {
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
               Gods
             </span>
+          </h1>
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
               Wave
